a32b7835cf2c35c40057d494400880d05f370612
xwx1000hours <xwx15655398209@163.com>
2021-04-24 09:59:06 +0800

    update

--- ch2.go
+++ ch2.go
@@ -7,7 +7,9 @@ import (
 	"github.com/pkg/errors"
 )
 var db *sql.DB
-
+/**
+初始化连接数据库
+*/
 func init() {
 	var err error
 	db, err  = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/blog")
@@ -16,16 +18,23 @@ func init() {
 	}
 }
 
-func getUser(db *sql.DB, arg string) (int, error){
+/**
+获取用户ID
+*/
+func getUserId(db *sql.DB, arg string) (int, error){
 	var id int
 	err := db.QueryRow("SELECT id FROM users WHERE email = ?",arg).Scan(&id)
-	return id, errors.Wrap(err,"The user was not found")
+	if err != sql.ErrNoRows {
+		return id, errors.Wrap(err,"The user was not found")
+	}
+	return id, nil
 }
 
 func main() {
 	defer db.Close()
-	id , err1 := getUser(db, "875198125@qq.com")
-	if !errors.Is(err1, sql.ErrNoRows) {
+	id , err1 := getUserId(db, "875198125@qq.com")
+
+	if err1 != nil {
 		fmt.Printf("Stack Trace:\n%+v\n", err1)
 	}
 	fmt.Println(id)
